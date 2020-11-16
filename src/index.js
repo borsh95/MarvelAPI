c9c67409e2b817c019add5f42b2e59030c85a359
ArtBro <artem.borsh95@gmail.com>
2020-11-16 12:05:26 +0300

    axios require

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import 'regenerator-runtime/runtime';
+import { API_URL, URL_COMICS, URL_CHARACTERS } from './js/constans/api';
+import { getDataApi } from './js/utils/GetDataApi';
 
-import axios from 'axios'
-
-const API_KEY = 'a5837db97d72016c81a7a776f4240db9';
-
-const url = "https://gateway.marvel.com/v1/public/comics";
-
-class GetDataApi {
-	async getData(url) {
-		const response = await axios.get(url, {
-			params: {
-				apikey: API_KEY,
-				limit: 100
-			}
-		});
-
-		return response.data.data.response;
-	}
-}
-
-const getApi = new GetDataApi();
-
-(async () => {
-	const data = await getApi.getData(url);
-	console.log(data);
+(async function () {
+	const data = await getDataApi.getData(API_URL + URL_COMICS);
+	console.log("Рузультат", data);
 })();
